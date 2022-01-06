0428f9115592f6d33a5140cad9527cec8c4e9d0b
Brooke Strand <brooke.strand007@gmail.com>
2022-01-06 18:16:07 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "John Smith",
+    streetAddress: "123 main st."
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = { ...employee};
+      newObj[key]=value;
+      return newObj;
+        //Code to return new, updated menu object goes here
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmp = { ...employee};
+    delete newEmp[key];
+    return newEmp;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
